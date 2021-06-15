812a3835f20db3e807b7ff974b372fc80de50d2d
Kei <69491052+Kei-7777@users.noreply.github.com>
2021-06-15 09:48:06 +0900

    add input method

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import glob                        
-from PIL import Image                                             
+from PIL import Image 
+
+size = int(input())                                            
                                                                                 
 for name in glob.iglob('**/*.png', recursive=True):                             
     img = Image.open(name)
-    resize = img.resize((4, 4), Image.LANCZOS)
+    resize = img.resize((size, size), Image.LANCZOS)
     resize.save(out)
     print(name)
