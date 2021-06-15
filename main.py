f6c9d732fab0cdee1d0db8c876e7f7f46cf7117b
Kei <69491052+Kei-7777@users.noreply.github.com>
2021-06-15 09:50:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ size = int(input())
 for name in glob.iglob('**/*.png', recursive=True):                             
     img = Image.open(name)
     resize = img.resize((size, size), Image.LANCZOS)
+    out = size + "/" + name
     resize.save(out)
     print(name)
