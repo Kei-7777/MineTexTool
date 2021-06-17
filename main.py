c625037a56b84e0f209bb4bbe8b62c50ef4b6c51
Kei <69491052+Kei-7777@users.noreply.github.com>
2021-06-18 06:33:18 +0900

    1.0以上はOpenCVで処理する

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import glob          
 import os              
+import cv2
 from PIL import Image
 
 def md(path):
@@ -11,19 +12,38 @@ size = float(input())
                                                                                 
 for name in glob.glob('assets/**/*.png', recursive=True):                             
     img = Image.open(name)
-    height = int(img.height*size)
-    width = int(img.width*size)
-    if height < 1:
-        height = 1
-    if width < 1:
-        width = 1
-    resize = img.resize((width, height), Image.LANCZOS)
-    out = "out\\" + str(size) + "\\" + name
-    pathlist = out.split('\\')
-    del pathlist[-1]
-    mdpath = ""
-    for st in pathlist:
-        mdpath = mdpath + st + "\\"
-    md(mdpath)
-    resize.save(out)
-    print("resize " + str(size) + "x" + str(size) + " : " + name)
+    if size < 1.0:
+        height = int(img.height*size)
+        width = int(img.width*size)
+        if height < 1:
+            height = 1
+        if width < 1:
+            width = 1
+        resize = img.resize((width, height), Image.LANCZOS)
+        out = "out\\" + str(size) + "\\" + name
+        pathlist = out.split('\\')
+        del pathlist[-1]
+        mdpath = ""
+        for st in pathlist:
+            mdpath = mdpath + st + "\\"
+        md(mdpath)
+        resize.save(out)
+        print("resize Pillow->" + str(size) + "x" + str(size) + " : " + name)
+    else:
+        img = cv2.imread(name)
+        height = int(img.shape[0]*size)
+        width = int(img.shape[1]*size)
+        if height < 1:
+            height = 1
+        if width < 1:
+            width = 1
+        resize = cv2.resize(img , (width, height))
+        out = "out\\" + str(size) + "\\" + name
+        pathlist = out.split('\\')
+        del pathlist[-1]
+        mdpath = ""
+        for st in pathlist:
+            mdpath = mdpath + st + "\\"
+        md(mdpath)
+        cv2.imwrite(out, resize)
+        print("resize OpenCV->" + str(size) + "x" + str(size) + " : " + name)
