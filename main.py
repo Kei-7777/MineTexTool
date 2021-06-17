b0a13b5858d5686c199115a1d7a28532bbb004b2
Kei <69491052+Kei-7777@users.noreply.github.com>
2021-06-17 19:49:03 +0900

    Pillowじゃないと透過できないので修正

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import glob          
 import os              
-import cv2
+from PIL import Image
 
 def md(path):
     if not os.path.isdir(path):
@@ -10,14 +10,14 @@ def md(path):
 size = float(input())                                            
                                                                                 
 for name in glob.glob('assets/**/*.png', recursive=True):                             
-    img = cv2.imread(name)
-    height = int(img.shape[0]*size)
-    width = int(img.shape[1]*size)
+    img = Image.open(name)
+    height = int(img.height*size)
+    width = int(img.width*size)
     if height < 1:
         height = 1
     if width < 1:
         width = 1
-    resize = cv2.resize(img , (width, height))
+    resize = img.resize((width, height), Image.LANCZOS)
     out = "out\\" + str(size) + "\\" + name
     pathlist = out.split('\\')
     del pathlist[-1]
@@ -25,5 +25,5 @@ for name in glob.glob('assets/**/*.png', recursive=True):
     for st in pathlist:
         mdpath = mdpath + st + "\\"
     md(mdpath)
-    cv2.imwrite(out, resize)
+    resize.save(out)
     print("resize " + str(size) + "x" + str(size) + " : " + name)
