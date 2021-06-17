3e32685adff591b96c999acfa1090b8cd738f935
Kei <69491052+Kei-7777@users.noreply.github.com>
2021-06-17 19:46:00 +0900

    4x4だけじゃなく比率に変えるように

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import glob          
 import os              
-from PIL import Image
+import cv2
 
 def md(path):
     if not os.path.isdir(path):
         os.makedirs(path) 
         print("makedir: " + path)
 
-size = int(input())                                            
+size = float(input())                                            
                                                                                 
 for name in glob.glob('assets/**/*.png', recursive=True):                             
-    img = Image.open(name)
-    resize = img.resize((size, size), Image.LANCZOS)
+    img = cv2.imread(name)
+    height = int(img.shape[0]*size)
+    width = int(img.shape[1]*size)
+    if height < 1:
+        height = 1
+    if width < 1:
+        width = 1
+    resize = cv2.resize(img , (width, height))
     out = "out\\" + str(size) + "\\" + name
     pathlist = out.split('\\')
     del pathlist[-1]
@@ -19,5 +25,5 @@ for name in glob.glob('assets/**/*.png', recursive=True):
     for st in pathlist:
         mdpath = mdpath + st + "\\"
     md(mdpath)
-    resize.save(out)
+    cv2.imwrite(out, resize)
     print("resize " + str(size) + "x" + str(size) + " : " + name)
