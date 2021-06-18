343144a2001a0d6492bdf3828309b48d0c8413c4
Kei <69491052+Kei-7777@users.noreply.github.com>
2021-06-18 21:17:04 +0900

    Nearest Neighbor法に置換

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import glob          
 import os              
-import cv2
+import numpy as np
 from PIL import Image
 
 def md(path):
@@ -8,11 +8,11 @@ def md(path):
         os.makedirs(path) 
         print("makedir: " + path)
 
-size = float(input())                                            
+size = float(input())                                      
                                                                                 
 for name in glob.glob('assets/**/*.png', recursive=True):                             
-    img = Image.open(name)
     if size < 1.0:
+        img = Image.open(name)
         height = int(img.height*size)
         width = int(img.width*size)
         if height < 1:
@@ -30,15 +30,9 @@ for name in glob.glob('assets/**/*.png', recursive=True):
         resize.save(out)
         print("resize Pillow->" + str(size) + "x" + str(size) + " : " + name)
     else:
-        img = cv2.imread(name)
-        height = int(img.shape[0]*size)
-        width = int(img.shape[1]*size)
-        if height < 1:
-            height = 1
-        if width < 1:
-            width = 1
-        # 段階的にResize
-        resize = cv2.resize(img , (width, height))
+        image = np.asarray(Image.open(name).convert("RGB"), dtype=np.uint8)
+        zoomed_image = image.repeat(size, axis=0).repeat(size, axis=1)
+        resize = Image.fromarray(zoomed_image)
         out = "out\\" + str(size) + "\\" + name
         pathlist = out.split('\\')
         del pathlist[-1]
@@ -46,5 +40,5 @@ for name in glob.glob('assets/**/*.png', recursive=True):
         for st in pathlist:
             mdpath = mdpath + st + "\\"
         md(mdpath)
-        cv2.imwrite(out, resize)
-        print("resize OpenCV->" + str(size) + "x" + str(size) + " : " + name)
+        resize.save(out)
+        print("resize Numpy->" + str(size) + "x" + str(size) + " : " + name)
